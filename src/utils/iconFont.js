f90ec25358c832d090aa39e74216c1ca8e63cff6
Trrrrw <wzhhenry@qq.com>
2025-05-16 14:48:41 +0800

    New: 崩坏3 视频

--- src/utils/iconFont.js
+++ src/utils/iconFont.js
@@ -1,5 +1,5 @@
 import { createFromIconfontCN } from '@ant-design/icons-vue'
 
 export const IconFont = createFromIconfontCN({
-    scriptUrl: '//at.alicdn.com/t/c/font_4881579_iuc6xt9019o.js',
+    scriptUrl: '//at.alicdn.com/t/c/font_4881579_lh3ttv52hui.js',
 })
