5344d5c073d22cc183a228673fc5b3fd0996d59d
Trrrrw <wzhhenry@qq.com>
2025-07-10 22:10:03 +0800

    Fix: title

--- src/utils/formatTitle.js
+++ src/utils/formatTitle.js
@@ -2,6 +2,7 @@ export const formatTitle = (title, game, type) => {
     const titleList = [
         '《绝区零》×《街霸6》制作人对谈',
         '《崩坏：星穹铁道》× Fate_UBW 联动预告',
+        '《崩坏：星穹铁道》× Fate[UBW] 联动PV：「相见『很』晚」',
     ]
     if (titleList.includes(title) || game == '崩坏3') return title
     title = title
