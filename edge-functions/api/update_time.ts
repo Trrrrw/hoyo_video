0b2aa119c9cc139bd4451ad8fa220eeed2b65035
Trrrrw <wzhhenry@qq.com>
2025-10-25 13:16:29 +0800

    Fix: 10251316

--- edge-functions/api/update_time.ts
+++
@@ -1,9 +0,0 @@
-export async function onRequestGet(): Promise<Response> {
-    const results = await get_update_time()
-    return results
-}
-
-async function get_update_time() {
-    const response = await fetch('https://api.trrw.tech/hoyo_video/update_time')
-    return response
-}
