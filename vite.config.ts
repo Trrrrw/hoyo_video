a3edbffe8bfccb23bd0d763caf579f71ae0e2232
Trrrrw <wzhhenry@qq.com>
2025-10-25 12:06:32 +0800

    Fix: 10251206

--- vite.config.ts
+++ vite.config.ts
@@ -36,13 +36,4 @@ export default defineConfig({
             '@': fileURLToPath(new URL('./src', import.meta.url))
         },
     },
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    vidstack: ['vidstack'],
-                },
-            },
-        },
-    },
 })
