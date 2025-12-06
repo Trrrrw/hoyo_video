756664aa5f3f8f26a10250b9ea2317807639026f
Trrrrw <wzhhenry@qq.com>
2025-12-06 19:38:54 +0800

    update: 更新500错误页面，新增服务状态按钮

--- src/pages/AppServerError.vue
+++ src/pages/AppServerError.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import AppEmpty from '@/components/AppEmpty.vue';
-import { useRouter } from 'vue-router'
-const router = useRouter()
+import AppEmpty from '@/components/AppEmpty.vue'
+import { h } from 'vue'
+import { GithubFilled, HddOutlined } from '@ant-design/icons-vue'
 </script>
 
 <template>
@@ -10,8 +10,13 @@ const router = useRouter()
             <app-empty description="" :imageStyle="{ height: '200px' }" />
         </template>
         <template #extra>
-            <a-button type="primary" href="https://github.com/Trrrrw/hoyo_video/issues/new" target="_blank">GitHub
-                Issues</a-button>
+                <a-button type="primary" :icon="h(GithubFilled)" href="https://github.com/Trrrrw/hoyo_video/issues/new"
+                    target="_blank">
+                    GitHub Issues
+                </a-button>
+                <a-button :icon="h(HddOutlined)" href="https://stats.trrw.tech/status/trrrrw" target="_blank">
+                    服务状态
+                </a-button>
         </template>
     </a-result>
 </template>
